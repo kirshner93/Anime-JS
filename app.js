f96418962ab76c2953b56c4415e53467980aac19
Vadik <kirshner.ua@gmail.com>
2022-01-19 22:06:23 +0300

    first commit

--- app.js
+++ app.js
@@ -1,10 +1,44 @@
-const moveTo = new MoveTo();
+var animation = anime.timeline({
+    targets: '.params-inheritance-demo .el',
+    delay: function(el, i) { return i * 200 },
+    duration: 800, // Can be inherited
+    easing: 'easeInQuad', // Can be inherited
+    direction: 'alternate', // Is not inherited
+    loop: false // Is not inherited
+  });
 
-const button = document.querySelectorAll('button');
-button.forEach(btn => moveTo.registerTrigger(btn))
+  document.querySelector('#btn').addEventListener('click', f1)
 
-const link = document.querySelectorAll('a')
-link.forEach(btn => moveTo.registerTrigger(btn))
+  function f1() {
+    animation
+    .add({
+        targets: '#moon',
+        top: '110px',
+        duration: 1110,
+        easing: 'spring(1, 80, 10, 0)'
+        
+        
+    })
+    .add({
+        targets: '#stars',
+        top: '0px',
+        
+        
+    })
+    .add({
 
+        targets: '#mountains_behind',
+        bottom: '0px',
+        easing: 'spring(1, 80, 10, 0)'
+        
+        
+    })
+    .add({
+        targets: '#mountains_front',
+        bottom: '0px',
+        easing: 'spring(1, 80, 10, 0)'
+        
 
-
+    })
+  }
+ 
